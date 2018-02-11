fea7bd015b1c7b647f7daa2081e9c429f2fd67db
shivapbhusal <civabhusal@gmail.com>
2018-02-11 08:44:28 -0500

    add lists

--- Scanner/scanner.py
+++ Scanner/scanner.py
@@ -29,53 +29,57 @@ class DFA:
 		self.goToInitial()
 		for inputs in inputList:
 			self.stateTransition(inputs)
-			continue 
 		return self.acceptState()
 
 
 states={0,1,2,3}
 alphabet={'a','b','c','d'}
-tf=dict()
+tranFunction=dict()
 
-tf[(0, 'a')] = 1
+tranFunction[(0, 'a')] = 1
 
-tf[(0, 'b')] = 2
+tranFunction[(0, 'b')] = 2
 
-tf[(0, 'c')] = 3
+tranFunction[(0, 'c')] = 3
 
-tf[(0, 'd')] = 0
+tranFunction[(0, 'd')] = 0
 
-tf[(1, 'a')] = 1
+tranFunction[(1, 'a')] = 1
 
-tf[(1, 'b')] = 2
+tranFunction[(1, 'b')] = 2
 
-tf[(1, 'c')] = 3
+tranFunction[(1, 'c')] = 3
 
-tf[(1, 'd')] = 0
+tranFunction[(1, 'd')] = 0
 
-tf[(2, 'a')] = 1
+tranFunction[(2, 'a')] = 1
 
-tf[(2, 'b')] = 2
+tranFunction[(2, 'b')] = 2
 
-tf[(2, 'c')] = 3
+tranFunction[(2, 'c')] = 3
 
-tf[(2, 'd')] = 0
+tranFunction[(2, 'd')] = 0
 
-tf[(3, 'a')] = 1
+tranFunction[(3, 'a')] = 1
 
-tf[(3, 'b')] = 2
+tranFunction[(3, 'b')] = 2
 
-tf[(3, 'c')] = 3
+tranFunction[(3, 'c')] = 3
 
-tf[(3, 'd')] = 0
+tranFunction[(3, 'd')] = 0
 
 start_state = 0
 
 accept_states = {2, 3}
 
-d = DFA(states, alphabet, tf, start_state, accept_states)
-inp_program = list('a')
-print (d.runInputList(inp_program))
+d = DFA(states, alphabet, tranFunction, start_state, accept_states)
+inputString = [list('ab'),list('abcd'),list('abcdef'),list('aaa'),list('bcde')]
+
+for strings in inputString:
+	if d.runInputList(strings)==2 or d.runInputList(strings)==3:
+		print(str(strings)+" "+"Accepted")
+	else:
+		print(str(strings)+" "+"Rejected")
 
 
 
